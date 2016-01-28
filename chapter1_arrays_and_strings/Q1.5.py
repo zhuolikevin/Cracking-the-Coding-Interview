c16505de0dd24d8f5fb5f826f65a3b4aba9eed66
Zhuo li <lizhuogo@gmail.com>
2016-01-28 15:07:20 +0800

    new solution

---
+++ chapter1_arrays_and_strings/Q1.5.py
@@ -0,0 +1,54 @@
+def compress(inputString):
+    # O(n) time and O(n) space
+    if not inputString: return ''
+
+    size = countCompress(inputString)
+    if size >= len(inputString):
+        return inputString
+
+    charArray = [''] * size
+    last, count, index = inputString[0], 1, 0
+    for i in range(1, len(inputString)):
+        if inputString[i] == last:
+            count += 1
+        else:
+            index = setChar(charArray, index, count, last)
+            last = inputString[i]
+            count = 1
+    setChar(charArray, index, count, last)
+    return ''.join(charArray)
+
+def countCompress(inputString):
+    count, last, size = 1, inputString[0], 0
+    for i in range(1, len(inputString)):
+        if inputString[i] == last:
+            count += 1
+        else:
+            size += 1 + len(str(count))
+            count = 1
+            last = inputString[i]
+    size += 1 + len(str(count))
+    return size
+
+def setChar(charArray, index, count, last):
+    charArray[index] = last
+    index += 1
+    for i in range(len(str(count))):
+        charArray[index] = str(count)[i]
+        index += 1
+    return index
+
+inputString = ''
+print compress(inputString)
+
+inputString = 'abcde'
+print compress(inputString)
+
+inputString = 'aabccccaaa'
+print compress(inputString)
+
+inputString = 'abccdee'
+print compress(inputString)
+
+inputString = 'accc'
+print compress(inputString)
